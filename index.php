fcc4fb5d9e81e15d0575e61d11b0bc33194cac41
master <mikahikaai@gmail.com>
2023-06-14 04:26:27 +0800

    fix minor bugs

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ if (!isset($_SESSION['level'])) {
   echo '<meta http-equiv="refresh" content="0;url=/login.php">';
   die();
 } else {
-  if ($_SESSION['level'] == "owner") {
+  if ($_SESSION['level'] == "Owner") {
     echo '<meta http-equiv="refresh" content="0;url=owner/"/>';
     die();
-  } else if ($_SESSION['level'] == "admin") {
+  } else if ($_SESSION['level'] == "Admin") {
     echo '<meta http-equiv="refresh" content="0;url=admnin/"/>';
     die();
-  } else if ($_SESSION['level'] == "kasir") {
+  } else if ($_SESSION['level'] == "Kasir") {
     echo '<meta http-equiv="refresh" content="0;url=kasir/"/>';
     die();
   }
